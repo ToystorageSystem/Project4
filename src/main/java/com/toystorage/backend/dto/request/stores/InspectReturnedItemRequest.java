84a367bb3a41910df72d8a30fa9c8e50a82e7397
Dinhlam0308 <dinhlam030805@gmail.com>
2026-08-20 01:55:12 +0700

    RETURNED GOODS CHECK

---
+++ src/main/java/com/toystorage/backend/dto/request/stores/InspectReturnedItemRequest.java
@@ -0,0 +1,39 @@
+package com.toystorage.backend.dto.request.stores;
+
+import com.toystorage.backend.enums.stores.ReturnItemCondition;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InspectReturnedItemRequest {
+
+    @NotBlank(
+            message = "Product barcode is required"
+    )
+    private String productBarcode;
+
+
+    @NotNull(
+            message = "Received quantity is required"
+    )
+    @Min(
+            value = 0,
+            message = "Received quantity cannot be negative"
+    )
+    private Integer receivedQuantity;
+
+
+    @NotNull(
+            message = "Condition status is required"
+    )
+    private ReturnItemCondition conditionStatus;
+
+
+    private String note;
+}
